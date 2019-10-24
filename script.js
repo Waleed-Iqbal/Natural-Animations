a5c307d498b4e6c462f02366ead276ae99be2def
waleed-iqbal <ikhawajawaleed@gmail.com>
2019-10-25 00:50:49 +0500

    set up squash and stretch animation

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-(function () {
 
+  function play_squashAndStretch(e) {
+    document.querySelector('.section-squash-and-stretch .animating-box').classList.add('animate');
+  }
 
-
-}());
+  function stop_squashAndStretch(e) {
+    document.querySelector('.section-squash-and-stretch .animating-box').classList.remove('animate');
+  }
