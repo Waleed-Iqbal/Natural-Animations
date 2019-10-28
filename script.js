be7b98b6d2b41b3a81dc0332920e664951b79627
waleed-iqbal <ikhawajawaleed@gmail.com>
2019-10-29 02:09:55 +0500

    improve code formatting

--- script.js
+++ script.js
@@ -1,102 +1,102 @@
 
-  function play_squashAndStretch(e) {
-    document.querySelector('.section-squash-and-stretch .animating-box').classList.add('animate');
-  }
+function play_squashAndStretch(e) {
+  document.querySelector('.section-squash-and-stretch .animating-box').classList.add('animate');
+}
 
-  function stop_squashAndStretch(e) {
-    document.querySelector('.section-squash-and-stretch .animating-box').classList.remove('animate');
-  }
-
-
-  var anticipationBox = ".section-anticipation .animating-box";
-
-  function play_anticipation(e) {
-    document.querySelector(anticipationBox).classList.add('animate');
-  }
-
-  function stop_anticipation(e) {
-    document.querySelector(anticipationBox).classList.remove('animate');
-  }
-
-  function play_staging(e) {
-    document.querySelector('.section-staging .animating-box').classList.add('animate');
-  }
-
-  function stop_staging(e) {
-    document.querySelector('.section-staging .animating-box').classList.remove('animate');
-  }
-
-  function play_straightAhead(e) {
-    document.querySelector('.section-straight-ahead .animating-box').classList.add('animate');
-  }
-
-  function stop_straightAhead(e) {
-    document.querySelector('.section-straight-ahead .animating-box').classList.remove('animate');
-  }
-
-  function play_followThrough(e) {
-    document.querySelector('.section-follow-through .animating-box').classList.add('animate');
-  }
-
-  function stop_followThrough(e) {
-    document.querySelector('.section-follow-through .animating-box').classList.remove('animate');
-  }
-
-  function play_slowInOut(e) {
-    document.querySelector('.section-slow-in-out .animating-box').classList.add('animate');
-  }
-
-  function stop_slowInOut(e) {
-    document.querySelector('.section-slow-in-out .animating-box').classList.remove('animate');
-  }
-
-  function play_arc(e) {
-    document.querySelector('.section-arc .animating-box').classList.add('animate');
-  }
-
-  function stop_arc(e) {
-    document.querySelector('.section-arc .animating-box').classList.remove('animate');
-  }
+function stop_squashAndStretch(e) {
+  document.querySelector('.section-squash-and-stretch .animating-box').classList.remove('animate');
+}
 
 
-  function play_secondaryAction(e) {
-    document.querySelector('.section-secondary-action .animating-box').classList.add('animate');
-  }
+var anticipationBox = ".section-anticipation .animating-box";
 
-  function stop_secondaryAction(e) {
-    document.querySelector('.section-secondary-action .animating-box').classList.remove('animate');
-  }
+function play_anticipation(e) {
+  document.querySelector(anticipationBox).classList.add('animate');
+}
+
+function stop_anticipation(e) {
+  document.querySelector(anticipationBox).classList.remove('animate');
+}
+
+function play_staging(e) {
+  document.querySelector('.section-staging .animating-box').classList.add('animate');
+}
+
+function stop_staging(e) {
+  document.querySelector('.section-staging .animating-box').classList.remove('animate');
+}
+
+function play_straightAhead(e) {
+  document.querySelector('.section-straight-ahead .animating-box').classList.add('animate');
+}
+
+function stop_straightAhead(e) {
+  document.querySelector('.section-straight-ahead .animating-box').classList.remove('animate');
+}
+
+function play_followThrough(e) {
+  document.querySelector('.section-follow-through .animating-box').classList.add('animate');
+}
+
+function stop_followThrough(e) {
+  document.querySelector('.section-follow-through .animating-box').classList.remove('animate');
+}
+
+function play_slowInOut(e) {
+  document.querySelector('.section-slow-in-out .animating-box').classList.add('animate');
+}
+
+function stop_slowInOut(e) {
+  document.querySelector('.section-slow-in-out .animating-box').classList.remove('animate');
+}
+
+function play_arc(e) {
+  document.querySelector('.section-arc .animating-box').classList.add('animate');
+}
+
+function stop_arc(e) {
+  document.querySelector('.section-arc .animating-box').classList.remove('animate');
+}
+
+
+function play_secondaryAction(e) {
+  document.querySelector('.section-secondary-action .animating-box').classList.add('animate');
+}
+
+function stop_secondaryAction(e) {
+  document.querySelector('.section-secondary-action .animating-box').classList.remove('animate');
+}
 
 
 
-  function play_timing(e) {
-    document.querySelector('.section-timing .animating-box').classList.add('animate');
-  }
+function play_timing(e) {
+  document.querySelector('.section-timing .animating-box').classList.add('animate');
+}
 
-  function stop_timing(e) {
-    document.querySelector('.section-timing .animating-box').classList.remove('animate');
-  }
+function stop_timing(e) {
+  document.querySelector('.section-timing .animating-box').classList.remove('animate');
+}
 
-  function play_exaggeration(e) {
-    document.querySelector('.section-exaggeration .animating-box').classList.add('animate');
-  }
+function play_exaggeration(e) {
+  document.querySelector('.section-exaggeration .animating-box').classList.add('animate');
+}
 
-  function stop_exaggeration(e) {
-    document.querySelector('.section-exaggeration .animating-box').classList.remove('animate');
-  }
+function stop_exaggeration(e) {
+  document.querySelector('.section-exaggeration .animating-box').classList.remove('animate');
+}
 
-  function play_solidDrawing(e) {
-    document.querySelector('.section-solid-drawing .animating-box').classList.add('animate');
-  }
+function play_solidDrawing(e) {
+  document.querySelector('.section-solid-drawing .animating-box').classList.add('animate');
+}
 
-  function stop_solidDrawing(e) {
-    document.querySelector('.section-solid-drawing .animating-box').classList.remove('animate');
-  }
+function stop_solidDrawing(e) {
+  document.querySelector('.section-solid-drawing .animating-box').classList.remove('animate');
+}
 
-  function play_Appeal(e) {
-    document.querySelector('.section-appeal .animating-box').classList.add('animate');
-  }
+function play_Appeal(e) {
+  document.querySelector('.section-appeal .animating-box').classList.add('animate');
+}
 
-  function stop_Appeal(e) {
-    document.querySelector('.section-appeal .animating-box').classList.remove('animate');
-  }
+function stop_Appeal(e) {
+  document.querySelector('.section-appeal .animating-box').classList.remove('animate');
+}
