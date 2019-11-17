547ba7f599d64b9c8468a967447724bbaf08eef0
waleed-iqbal <ikhawajawaleed@gmail.com>
2019-11-18 00:58:46 +0500

    clean dist folder each time when built

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,15 @@
 const path = require('path');
 const common = require('./webpack.common');
 const merge = require('webpack-merge');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // clean the dist folder to avoid having multiple bundles being generated
 
 module.exports = merge(common, {
   mode: 'production',
   output: {
-   filename:  "./index-[contentHash].js",
-   path: path.resolve(__dirname, 'dist')
-  }
+    filename: "./index-[contentHash].js",
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+  ],
 });
