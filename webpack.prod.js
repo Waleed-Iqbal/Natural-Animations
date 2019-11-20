711614b9f606363a1b878049d635484732fa3ce9
waleed-iqbal <ikhawajawaleed@gmail.com>
2019-11-20 23:46:12 +0500

    optimize css and js for production

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,6 +3,8 @@ const common = require('./webpack.common');
 const merge = require('webpack-merge');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // clean the dist folder to avoid having multiple bundles being generated
 const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // to have separate css files instead on injecting it via JS bundle
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
@@ -10,12 +12,18 @@ module.exports = merge(common, {
     filename: "./index-[contentHash].js",
     path: path.resolve(__dirname, 'dist')
   },
+  optimization: {
+    minimizer: [
+      new TerserPlugin(), // for JS
+      new OptimizeCssAssetsPlugin() // for CSS
+    ]
+  },
   module: {
     rules: [
       {
         test: /\.scss$/,
         use: [
-          MiniCssExtractPlugin.loader, // extract css into files
+          MiniCssExtractPlugin.loader, // extract css into files and minify it
           'css-loader', // converts css to js
           'sass-loader' // converts scss to css
         ]
@@ -24,7 +32,7 @@ module.exports = merge(common, {
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: '[name].[contentHash].css'
+      filename: 'styles.[contentHash].css'
     }),
     new CleanWebpackPlugin(),
   ],
